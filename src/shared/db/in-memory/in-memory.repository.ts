739471178b51a572bbbb60600d76791d914b2651
Dioner <dionerweiss@gmail.com>
2023-12-29 01:55:14 -0300

    feat: create in-memory repository

---
+++ src/shared/db/in-memory/in-memory.repository.ts
@@ -0,0 +1,55 @@
+import { Entity } from "../../domain/entity";
+import { IRepository } from "../../domain/repository/repository.interface";
+import { ValueObject } from "../../domain/value-object";
+
+export abstract class InMemory<E extends Entity, EntityId extends ValueObject>
+  implements IRepository<E, EntityId>
+{
+  items: E[] = [];
+
+  async insert(entity: E): Promise<void> {
+    this.items.push(entity);
+  }
+
+  async bulkInsert(entities: E[]): Promise<void> {
+    this.items.push(...entities);
+  }
+
+  async update(entity: E): Promise<void> {
+    const indexFound = this.items.findIndex((item) =>
+      item.entity_id.equals(entity.entity_id)
+    );
+
+    if (indexFound === -1) {
+      throw new Error("Entity not found");
+    }
+
+    this.items[indexFound] = entity;
+  }
+
+  async delete(entity_id: EntityId): Promise<void> {
+    const indexFound = this.items.findIndex((item) =>
+      item.entity_id.equals(entity_id)
+    );
+    if (indexFound === -1) {
+      throw new Error("Entity not found");
+    }
+
+    this.items.splice(indexFound, 1);
+  }
+
+  async findById(entity_id: EntityId): Promise<E | null> {
+    return this._get(entity_id);
+  }
+
+  async findAll(): Promise<E[]> {
+    return this.items;
+  }
+
+  abstract getEntity(): new (...args: any[]) => E;
+
+  protected async _get(entity_id: EntityId) {
+    const item = this.items.find((item) => item.entity_id.equals(entity_id));
+    return typeof item === "undefined" ? null : item;
+  }
+}
