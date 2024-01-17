b2aff8e557f1eb5339c57d48dc2a013093769a43
Dioner <dionerweiss@gmail.com>
2024-01-16 22:03:54 -0300

    feat: search method on category repository

--- src/category/infra/db/sequelize/category-sequelize.repository.ts
+++ src/category/infra/db/sequelize/category-sequelize.repository.ts
@@ -1,9 +1,12 @@
-import { Entity } from "../../../../shared/domain/entity";
+import { Op } from "sequelize";
+import { NotFoundError } from "../../../../shared/domain/errors/not-found.error";
 import { SearchParams } from "../../../../shared/domain/repository/search-params";
-import { SearchResult } from "../../../../shared/domain/repository/search-result";
 import { Uuid } from "../../../../shared/domain/value-objects/uuid.vo";
 import { Category } from "../../../domain/category.entity";
-import { ICategoryRepository } from "../../../domain/category.repository";
+import {
+  CategorySearchResult,
+  ICategoryRepository,
+} from "../../../domain/category.repository";
 import { CategoryModel } from "./category.model";
 
 export class CategorySequelizeRepository implements ICategoryRepository {
@@ -33,16 +36,36 @@ export class CategorySequelizeRepository implements ICategoryRepository {
     );
   }
 
-  update(entity: Category): Promise<void> {
-    throw new Error("Method not implemented.");
+  async update(entity: Category): Promise<void> {
+    const id = entity.category_id.id;
+    const model = await this._get(id);
+    if (!model) {
+      throw new NotFoundError(id, this.getEntity());
+    }
+    await this.categoryModel.update(
+      {
+        category_id: entity.category_id.id,
+        name: entity.name,
+        description: entity.description,
+        is_active: entity.is_active,
+        created_at: entity.created_at,
+      },
+      { where: { category_id: id } }
+    );
   }
 
-  delete(entity_id: Uuid): Promise<void> {
-    throw new Error("Method not implemented.");
+  async delete(entity_id: Uuid): Promise<void> {
+    const id = entity_id.id;
+    const model = await this._get(id);
+    if (!model) {
+      throw new NotFoundError(id, this.getEntity());
+    }
+
+    await this.categoryModel.destroy({ where: { category_id: id } });
   }
 
   async findById(entity_id: Uuid): Promise<Category | null> {
-    const model = await this.categoryModel.findByPk(entity_id.id);
+    const model = await this._get(entity_id.id);
     return new Category({
       category_id: new Uuid(model.category_id),
       name: model.name,
@@ -50,6 +73,12 @@ export class CategorySequelizeRepository implements ICategoryRepository {
       is_active: model.is_active,
       created_at: model.created_at,
     });
+  }
+
+  private async _get(id: string) {
+    {
+      return await this.categoryModel.findByPk(id);
+    }
   }
 
   async findAll(): Promise<Category[]> {
@@ -66,11 +95,39 @@ export class CategorySequelizeRepository implements ICategoryRepository {
     );
   }
 
-  search(props: SearchParams<string>): Promise<SearchResult<Entity>> {
-    throw new Error("Method not implemented.");
+  async search(props: SearchParams<string>): Promise<CategorySearchResult> {
+    const offset = (props.page - 1) * props.per_page;
+    const limit = props.per_page;
+    const { rows: models, count } = await this.categoryModel.findAndCountAll({
+      ...(props.filter && {
+        where: {
+          name: { [Op.like]: `%${props.filter}%` },
+        },
+      }),
+      ...(props.sort && this.sortableFields.includes(props.sort)
+        ? { order: [[props.sort, props.sort_dir]] }
+        : { order: [["created_at", "desc"]] }),
+      offset,
+      limit,
+    });
+    return new CategorySearchResult({
+      items: models.map(
+        (model) =>
+          new Category({
+            category_id: new Uuid(model.category_id),
+            name: model.name,
+            description: model.description,
+            is_active: model.is_active,
+            created_at: model.created_at,
+          })
+      ),
+      current_page: props.page,
+      per_page: props.per_page,
+      total: count,
+    });
   }
 
   getEntity(): new (...args: any[]) => Category {
-    throw new Error("Method not implemented.");
+    return Category;
   }
 }
