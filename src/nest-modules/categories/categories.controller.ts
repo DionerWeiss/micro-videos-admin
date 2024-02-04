63e0b26fc0036b0f7acf8e79546cb9edb96a9f20
Dioner <dionerweiss@gmail.com>
2024-02-04 19:55:25 -0300

    feat: serialize output on category create

--- src/nest-modules/categories/categories.controller.ts
+++ src/nest-modules/categories/categories.controller.ts
@@ -1,5 +1,11 @@
+import { CategoryOutput } from '@core/category/application/use-cases/common/category-output';
 import { CreateCategoryUseCase } from '@core/category/application/use-cases/create-category/create-category.use-case';
+import { DeleteCategoryUseCase } from '@core/category/application/use-cases/delete-category/delete-category.use-case';
+import { GetCategoryUseCase } from '@core/category/application/use-cases/get-category/get-category.use-case';
+import { ListCategoriesUseCase } from '@core/category/application/use-cases/list-categories/list-categories.use-case';
+import { UpdateCategoryUseCase } from '@core/category/application/use-cases/update-category/update-category.use-case';
 import { Body, Controller, Inject, Post } from '@nestjs/common';
+import { CategoryPresenter } from 'src/nest-modules/categories/categories.presenter';
 import { CreateCategoryDto } from './dto/create-category.dto';
 
 @Controller('categories')
@@ -7,8 +13,25 @@ export class CategoriesController {
   @Inject(CreateCategoryUseCase)
   private createUseCase: CreateCategoryUseCase;
 
+  @Inject(UpdateCategoryUseCase)
+  private updateUseCase: UpdateCategoryUseCase;
+
+  @Inject(DeleteCategoryUseCase)
+  private deleteUseCase: DeleteCategoryUseCase;
+
+  @Inject(GetCategoryUseCase)
+  private getUseCase: GetCategoryUseCase;
+
+  @Inject(ListCategoriesUseCase)
+  private listUseCase: ListCategoriesUseCase;
+
   @Post()
   async create(@Body() createCategoryDto: CreateCategoryDto) {
-    return this.createUseCase.execute(createCategoryDto);
+    const output = await this.createUseCase.execute(createCategoryDto);
+    return CategoriesController.serialize(output);
+  }
+
+  static serialize(output: CategoryOutput) {
+    return new CategoryPresenter(output);
   }
 }
