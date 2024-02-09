608548ba2b8ef6d0a3be90e1045b352d4204bac4
Dioner <dionerweiss@gmail.com>
2024-02-09 01:27:46 -0300

    feat: create cast members nest module

---
+++ src/nest-modules/cast-members/dto/search-cast-members.dto.ts
@@ -0,0 +1,3 @@
+import { ListCastMembersInput } from '@core/cast-member/application/use-cases/list-cast-members/list-cast-members.use-case';
+
+export class SearchCastMemberDto implements ListCastMembersInput {}
