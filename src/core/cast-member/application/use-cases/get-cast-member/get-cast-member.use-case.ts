eaadbc145e2bbadceb250134c17f95e8fa3eb01d
Dioner <dionerweiss@gmail.com>
2024-02-09 00:56:05 -0300

    feat: get cast member

---
+++ src/core/cast-member/application/use-cases/get-cast-member/get-cast-member.use-case.ts
@@ -0,0 +1,33 @@
+import {
+  CastMemberOutput,
+  CastMemberOutputMapper,
+} from '@core/cast-member/application/use-cases/common/cast-member-output';
+import {
+  CastMember,
+  CastMemberId,
+} from '@core/cast-member/domain/cast-member.aggregate';
+import { ICastMemberRepository } from '@core/cast-member/domain/cast-member.repository';
+import { IUseCase } from '@core/shared/application/use-case.interface';
+import { NotFoundError } from '@core/shared/domain/errors/not-found.error';
+
+export class GetCastMemberUseCase
+  implements IUseCase<GetCastMemberInput, GetCastMemberOutput>
+{
+  constructor(private castMemberRepo: ICastMemberRepository) {}
+
+  async execute(input: GetCastMemberInput): Promise<GetCastMemberOutput> {
+    const castMemberId = new CastMemberId(input.id);
+    const castMember = await this.castMemberRepo.findById(castMemberId);
+    if (!castMember) {
+      throw new NotFoundError(input.id, CastMember);
+    }
+
+    return CastMemberOutputMapper.toOutput(castMember);
+  }
+}
+
+export type GetCastMemberInput = {
+  id: string;
+};
+
+export type GetCastMemberOutput = CastMemberOutput;
