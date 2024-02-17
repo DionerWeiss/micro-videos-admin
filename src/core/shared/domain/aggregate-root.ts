2eba3844fe0e07d1f9b6c4092990f3b6942c7d24
Dioner <dionerweiss@gmail.com>
2024-02-17 01:47:04 -0300

    feat: aggregate root events

--- src/core/shared/domain/aggregate-root.ts
+++ src/core/shared/domain/aggregate-root.ts
@@ -1,3 +1,18 @@
 import { Entity } from '@core/shared/domain/entity';
+import { IDomainEvent } from '@core/shared/events/domain-event.interface';
 
-export abstract class AggregateRoot extends Entity {}
+import EventEmitter2 from 'eventemitter2';
+
+export abstract class AggregateRoot extends Entity {
+  events: Set<IDomainEvent> = new Set<IDomainEvent>();
+  localMediator = new EventEmitter2();
+
+  applyEvent(event: IDomainEvent) {
+    this.events.add(event);
+    this.localMediator.emit(event.constructor.name, event);
+  }
+
+  registerHandler(event: string, handler: (event: IDomainEvent) => void) {
+    this.localMediator.on(event, handler);
+  }
+}
